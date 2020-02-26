a4ba4494437639688f202db3d1e1100f610ee2f7
Danilo Nakabar Alencar <dnakabar@server01.foursys.local>
2020-02-26 18:22:16 -0300

    primeiro commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,24 @@
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$sFKmbxbG4ryhwPNx/l3pgOJSt.fW1z6YcUnuE2X8APA/Z3NI/oSpq');
+
+INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
+INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
+INSERT INTO CURSO(nome, categoria) VALUES('Angular', 'Programação');
+INSERT INTO CURSO(nome, categoria) VALUES('BootStrap', 'Front-end');
+INSERT INTO CURSO(nome, categoria) VALUES('NodeJS', 'Programação');
+INSERT INTO CURSO(nome, categoria) VALUES('JAVAEE', 'Programação');
+INSERT INTO CURSO(nome, categoria) VALUES('Hibernate', 'Programação');
+INSERT INTO CURSO(nome, categoria) VALUES('JDBC', 'Programação');
+INSERT INTO CURSO(nome, categoria) VALUES('CSS3', 'Front-end');
+
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 1', 'Erro ao criar projeto', '2019-05-05 18:00:00', 'NAO_RESPONDIDO', 1, 1);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 2', 'Projeto não compila', '2019-05-05 19:00:00', 'NAO_RESPONDIDO', 1, 1);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 3', 'Tag HTML', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', 1, 2);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 4', 'erro undefinied', '2019-05-05 21:00:00', 'NAO_RESPONDIDO', 1, 3);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 5', 'Tag container', '2019-05-05 17:00:00', 'NAO_RESPONDIDO', 1, 4);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 6', 'Docker AWS', '2019-05-05 10:00:00', 'NAO_RESPONDIDO', 1, 5);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 7', 'JavaBeans', '2019-05-05 23:00:00', 'NAO_RESPONDIDO', 1, 6);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 8', 'Usando Mysql', '2019-05-05 16:00:00', 'NAO_RESPONDIDO', 1, 7);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 9', 'Usando SQLSERVER', '2019-05-05 23:00:00', 'NAO_RESPONDIDO', 1, 7);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 10', 'concatenar SQL', '2019-05-05 12:00:00', 'NAO_RESPONDIDO', 1, 8);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 11', 'float-right', '2019-05-05 09:00:00', 'NAO_RESPONDIDO', 1, 9);
+
